979c2209aab6810e0aa2015f8141205f4266a972
wmjoers <35455045+wmjoers@users.noreply.github.com>
2023-04-28 07:13:36 +0200

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -2,27 +2,31 @@ import { Observable } from 'rxjs';
 
 console.log('App started');
 
-const observable$ = new Observable<number>(subscriber => {
-  console.log('Inside observable');
+const colsObservable$ = new Observable<number>(subscriber => {
   let value = 1;
-  
   const intervalId = setInterval(() => {
-    console.log('Emitting:',value);
     subscriber.next(value++);
   }, 1000);
-
-  return () => {
-    console.log('Teardown');
-    clearInterval(intervalId);
-  }
 });
 
-const subscription = observable$.subscribe({
-  next: value => console.log('Got value:', value),
-  complete: () => console.log('Completed'),
-  error: err => console.error('Got error:', err)
+
+const hotObservable$ = new Observable<string>(subscriber => {
+  const intervalId = setInterval(() => {
+    subscriber.next(new Date().toLocaleTimeString());
+  }, 1000);
+});
+
+colsObservable$.subscribe({
+  next: value => console.log('Got value A:', value),
+  complete: () => console.log('Completed A'),
+  error: err => console.error('Got error A:', err)
 });
 
 setTimeout(() => {
-  subscription.unsubscribe();
-}, 5000);
+  colsObservable$.subscribe({
+    next: value => console.log('Got value B:', value),
+    complete: () => console.log('Completed B'),
+    error: err => console.error('Got error B:', err)
+  });
+}, 3000);
+
