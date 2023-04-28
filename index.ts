febe8dc6de64311b2f44a011310bd91ba29e4cac
wmjoers <35455045+wmjoers@users.noreply.github.com>
2023-04-28 09:52:10 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,27 +1,2 @@
-import { Observable, zip, merge, forkJoin } from 'rxjs';
 
-const allProducts$ = new Observable<string>(subscriber => {
-  console.log('Fetching products');
-  let products = ['Product1', 'Product2', 'Product3'];
-  setTimeout(() => {
-    products.forEach(product => subscriber.next(product));
-    subscriber.complete();
-  }, 1000);
-});
-
-const allPrices$ = new Observable<number>(subscriber => {
-  console.log('Fetching prices');
-  let prices = [100, 200, 300];
-  setTimeout(() => {
-    prices.forEach(price => subscriber.next(price));
-    subscriber.complete();
-  }, 1000);
-});
-
-zip([allProducts$, allPrices$])
-.subscribe({
-  next: value => console.log('Got value:', value),
-  complete: () => console.log('Completed'),
-  error: err => console.error('Got error:', err)
-});
-
+console.log('Hello world!');
